ac5ffba7748ccfc8f4b6d9a288097cc4813e6e99
mbrik <m_brik@estin.dz>
2026-08-16 22:54:52 +0200

    feat: initial project structure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mbrik/CLI-Benchmarking-Tool
+
+go 1.26.4
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    fmt.Println("🚀 Initializing Go Benchmarking CLI Tool...")
+    // سنقوم لاحقاً بربطه بـ cmd و worker
+}
